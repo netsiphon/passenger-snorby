d7b51719eccd56e6158d204e1056c9c1b9f39714
Joseph Kennedy <joseph@netsiphon.com>
2016-09-14 03:26:10 -0400

    -

--- snorby/startup.sh
+++ snorby/startup.sh
@@ -4,16 +4,16 @@
 mysql -u $DB_USER -p $DB_PASSWORD -h $DB_HOST < $SNORBY_PATH/snorby_setup.sql
 
 #Modify Configs
-sed -i 's/$DB_HOST/'$DB_HOST'/g' /usr/local/src/snorby/config/database.yml
-sed -i 's/$DB_PORT/'$DB_PORT'/g' /usr/local/src/snorby/config/database.yml
-sed -i 's/$DB_USER/'$DB_USER'/g' /usr/local/src/snorby/config/database.yml
-sed -i 's/$DB_PASSWORD/'$DB_PASSWORD'/g' /usr/local/src/snorby/config/database.yml
+sed -i 's/$DB_HOST/'$DB_HOST'/g' $SNORBY_PATH/config/database.yml
+sed -i 's/$DB_PORT/'$DB_PORT'/g' $SNORBY_PATH/config/database.yml
+sed -i 's/$DB_USER/'$DB_USER'/g' $SNORBY_PATH/config/database.yml
+sed -i 's/$DB_PASSWORD/'$DB_PASSWORD'/g' $SNORBY_PATH/config/database.yml
 
 #Snort Rules at some point...
 
 #Setup Snorby
 cd $SNORBY_PATH
-bundle exec rake snorby:setup
+/bin/bash -l -c bundle exec rake snorby:setup
 
 SNORBY_START="rails server -e production"
 
@@ -22,5 +22,5 @@ cd $SNORBY_PATH
 $SNORBY_START
 
 #Delayed Jobs start
-rails runner "Snorby::Worker.start"
-rails runner "Snorby Cache Jobs"
+/bin/bash -l -c rails runner "Snorby::Worker.start"
+/bin/bash -l -c rails runner "Snorby Cache Jobs"
